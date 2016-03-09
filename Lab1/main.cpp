b2932179f9d125e4fb0d828582f999951b7c57b2
Shavakan <chiyah92@gmail.com>
2016-03-09 20:54:35 +0900

    Lab1 guideline copied

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -43,66 +43,114 @@ void koch_line(glm::vec3 a, glm::vec3 b, int iter)
 // TODO: Initialize model
 void init_model(void)
 {
+    g_vertex_buffer_data = std::vector<glm::vec3>();
 
+    g_vertex_buffer_data.push_back(glm::vec3(-0.5f, -0.25f, 0.0f));
+    g_vertex_buffer_data.push_back(glm::vec3(0.5f, -0.25f, 0.0f));
+    g_vertex_buffer_data.push_back(glm::vec3(0.0f, sqrt(0.75)-0.25f, 0.0f));
+
+    // Generates Vertex Array Objects in the GPU's memory and passes back their identifiers
+    // Create a vertex array object that represents vertex attributes stored in a vertex buffer object.
+    glGenVertexArrays(1, &VAID);
+    glBindVertexArray(VAID);
+
+    // Create and initialize a buffer object. Generates our buffers in the GPU's memory
+    glGenBuffers(1, &VBID);
+    glBindBuffer(GL_ARRAY_BUFFER, VBID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3)*g_vertex_buffer_data.size(),
+                 &g_vertex_buffer_data[0], GL_STATIC_DRAW);
 }
 
 // TODO: Draw model
 void draw_model()
 {
+    glUseProgram(programID);
+    glBindVertexArray(VAID);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBID);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), BUFFER_OFFSET(0));
 
+    glm::mat4 Model = glm::mat4(1.0f);
+    glm::mat4 MVP = Projection * View * Model;
+
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+    glDrawArrays(GL_TRIANGLES, 0, g_vertex_buffer_data.size());
+
+    glDisableVertexAttribArray(0);
 }
 
 int main(int argc, char* argv[])
 {
-	// Step 1: Initialization
-	if (!glfwInit())
-	{
-		return -1;
-	}
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // Step 1: Initialization
+    if (!glfwInit())
+    {
+        return -1;
+    }
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	// TODO: GLFW create window and context
+    // TODO: GLFW create window and context
+    window = glfwCreateWindow(1024, 768, "Lab 1", NULL, NULL);
+    if (window == NULL) {
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    // END
 
-	// END
+    // TODO: Initialize GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        return -1;
+    }
+    // END
 
-	// TODO: Initialize GLEW
+    Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+    View = glm::lookAt(glm::vec3(0, 0, 2),
+                                 glm::vec3(0, 0, 0),
+                                 glm::vec3(0, 1, 0));
+    glm::mat4 Model = glm::mat4(1.0f);
+    glm::mat4 MVP = Projection * View * Model;
 
-	// END
+    // TODO: Initialize OpenGL and GLSL
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
 
-	Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-	View = glm::lookAt(glm::vec3(0, 0, 2),
-				 				 glm::vec3(0, 0, 0),
-								 glm::vec3(0, 1, 0));
-	glm::mat4 Model = glm::mat4(1.0f);
-	glm::mat4 MVP = Projection * View * Model;
+    // For Macbook Pro Retina screen scaling
+    int width, height;
+    glfwGetFramebufferSize(window,&width, &height);
+    glViewport(0, 0, width, height);
 
-	// TODO: Initialize OpenGL and GLSL
+    programID = LoadShaders("VertexShader.glsl", "fragmentShader.glsl");
+    GLuint MatrixId = glGetUniformLocation(programID, "MVP");
+    // END
+    init_model();
 
-	// END
-	init_model();
 
-	// Step 2: Main event loop
-	do {
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // Step 2: Main event loop
+    do {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		draw_model();
+        draw_model();
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	} while (!glfwWindowShouldClose(window));
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while (!glfwWindowShouldClose(window));
 
-	// Step 3: Termination
-	g_vertex_buffer_data.clear();
+    // Step 3: Termination
+    g_vertex_buffer_data.clear();
 
-	glDeleteBuffers(1, &VBID);
-	glDeleteProgram(programID);
-	glDeleteVertexArrays(1, &VAID);
+    glDeleteBuffers(1, &VBID);
+    glDeleteProgram(programID);
+    glDeleteVertexArrays(1, &VAID);
 
-	glfwTerminate();
+    glfwTerminate();
 
-	return 0;
+    return 0;
 }
